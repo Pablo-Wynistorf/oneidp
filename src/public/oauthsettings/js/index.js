210e507b9f7524fc160ea6845d8235b3804b953e
Pablo-Wynistorf <p.wynistorf@gmail.com>
2024-02-25 14:15:37 +0100

    implemented dialog as confirmation box docker

--- src/public/oauthsettings/js/index.js
+++ src/public/oauthsettings/js/index.js
@@ -1,13 +1,13 @@
-const errorBox = document.createElement('div');
-const successBox = document.createElement('div');
+const errorBox = document.createElement("div");
+const successBox = document.createElement("div");
 
-errorBox.className = 'error-box';
-successBox.className = 'success-box';
+errorBox.className = "error-box";
+successBox.className = "success-box";
 
 function getCookie(name) {
-  const cookieArray = document.cookie.split(';');
+  const cookieArray = document.cookie.split(";");
   for (const cookie of cookieArray) {
-    const [cookieName, cookieValue] = cookie.trim().split('=');
+    const [cookieName, cookieValue] = cookie.trim().split("=");
     if (cookieName === name) {
       return cookieValue;
     }
@@ -15,35 +15,34 @@ function getCookie(name) {
   return null;
 }
 
-
 function create_app() {
-  const container = document.querySelector('.container');
-  const accessToken = getCookie('access_token');
-  const oauthAppName = document.getElementById('appname-field').value;
-  const redirectUri = document.getElementById('redirecturl-field').value;
+  const container = document.querySelector(".container");
+  const accessToken = getCookie("access_token");
+  const oauthAppName = document.getElementById("appname-field").value;
+  const redirectUri = document.getElementById("redirecturl-field").value;
 
   if (accessToken) {
     fetch(`/api/oauth/settings/add`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${accessToken}`
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
       },
-      body: JSON.stringify({ redirectUri, oauthAppName })
+      body: JSON.stringify({ redirectUri, oauthAppName }),
     })
-    .then(response => {
-      if (!response.ok) {
-        return handleResponse(response)
-      }
-      return response.json();
-    })
-    .then(data => {
-      const newAppBox = document.createElement('div');
-      newAppBox.classList.add('oauth-app-box');
-      newAppBox.id = data.oauthClientAppId;
-      
-      const appBoxHTML = `
-        <a href="#" class="icon-button" id="${data.oauthClientAppId}">
+      .then((response) => {
+        if (!response.ok) {
+          return handleResponse(response);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const newAppBox = document.createElement("div");
+        newAppBox.classList.add("oauth-app-box");
+        newAppBox.id = data.oauthClientAppId;
+
+        const appBoxHTML = `
+        <a class="icon-button" id="${data.oauthClientAppId}">
           <img src="./images/trash.svg" alt="Trash Icon">
         </a>
         <h4>APP NAME: ${data.oauthAppName}</h4>
@@ -52,101 +51,91 @@ function create_app() {
         <p>Client Secret: ${data.clientSecret}</p>
         <p>Redirect URI: ${data.redirectUri}</p>
       `;
-      
-      newAppBox.innerHTML = appBoxHTML;
 
-      const oauthAppInfo = container.querySelector('.oauth-app-info');
-      if (oauthAppInfo) {
-        oauthAppInfo.insertAdjacentElement('afterend', newAppBox);
-      } else {
-        container.appendChild(newAppBox);
-      }
-      displaySuccess('App created successfully');
-    })
-    .catch(error => {
-    });
+        newAppBox.innerHTML = appBoxHTML;
+
+        const oauthAppInfo = container.querySelector(".oauth-app-info");
+        if (oauthAppInfo) {
+          oauthAppInfo.insertAdjacentElement("afterend", newAppBox);
+        } else {
+          container.appendChild(newAppBox);
+        }
+        displaySuccess("App created successfully");
+      })
+      .catch((error) => {});
   }
 }
 
+const modal = document.querySelector("[data-modal]");
+const closeButton = document.querySelector("[data-close-modal]");
 
-document.addEventListener('click', function(event) {
-  const iconButton = event.target.closest('.icon-button');
+document.addEventListener("click", function (event) {
+  const iconButton = event.target.closest(".icon-button");
   if (iconButton) {
-    showConfirmationBox(event);
+    modal.showModal();
+    document.getElementById("delete-button").onclick = function () {
+      const appId = iconButton.id;
+      delete_app(appId);
+      modal.close();
+    };
   }
 });
 
-function showConfirmationBox(event) {
-  var confirmationBox = document.getElementById('confirmation-box');
-  confirmationBox.style.display = 'block';
-  document.getElementById('delete-button').onclick = function() {
-    delete_app(event);
-    hideConfirmationBox();
-  }
-}
+closeButton.addEventListener("click", function () {
+  modal.close();
+});
 
-function hideConfirmationBox() {
-  var confirmationBox = document.getElementById('confirmation-box');
-  confirmationBox.style.display = 'none';
-}
-
-
-
-function delete_app(event) {
-  event.preventDefault();
-  const oauthClientAppId = event.target.offsetParent.id;
-  const accessToken = getCookie('access_token');
+function delete_app(appId) {
+  const oauthClientAppId = appId;
+  const accessToken = getCookie("access_token");
   if (accessToken) {
     fetch(`/api/oauth/settings/delete`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${accessToken}`
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
       },
-      body: JSON.stringify({ oauthClientAppId })
-    }).then(response => {
-      if (response.ok) {
-        event.target.offsetParent.remove();
-        document.getElementById(oauthClientAppId).remove();
-        displaySuccess('App deleted successfully');
-      } else if (response.status === 460) {
-        displayError('Error: App Deletion Failed')
-      } else {
-        handleError();
-      }})
-    .catch(error => {
-      displayError('Something went wrong');
-    });
+      body: JSON.stringify({ oauthClientAppId }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          document.getElementById(oauthClientAppId).remove();
+          displaySuccess("App deleted successfully");
+        } else if (response.status === 460) {
+          displayError("Error: App Deletion Failed");
+        } else {
+          handleError();
+        }
+      })
+      .catch((error) => {
+        displayError("Something went wrong");
+      });
   }
 }
 
-
-
 async function fetchData() {
-    try {
-        const accessToken = getCookie('access_token');
-        const response = await fetch('/api/oauth/settings/get', {
-            headers: {
-                'Authorization': `Bearer ${accessToken}`
-            }
-        });
-        if (!response.ok) {
-          handleResponse(response)
-        }
-        const data = await response.json();
-        displayOAuthApps(data);
-    } catch (error) {
+  try {
+    const accessToken = getCookie("access_token");
+    const response = await fetch("/api/oauth/settings/get", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    if (!response.ok) {
+      handleResponse(response);
     }
+    const data = await response.json();
+    displayOAuthApps(data);
+  } catch (error) {}
 }
 
-
 function displayOAuthApps(data) {
-  const container = document.querySelector('.container');
-  data.oauthApps.forEach(app => {
-      const appBox = document.createElement('div');
-      appBox.innerHTML = `
+  const container = document.querySelector(".container");
+  data.oauthApps.forEach((app) => {
+    const appBox = document.createElement("div");
+    appBox.innerHTML = `
        <div class="oauth-app-box" id="${app.oauthClientAppId}">
-          <a href="#" class="icon-button" id="${app.oauthClientAppId}">
+          <a class="icon-button" id="${app.oauthClientAppId}">
           <img src="./images/trash.svg" alt="Trash Icon">
           </a>
           <h4>APP NAME: ${app.oauthAppName}</h4>
@@ -156,15 +145,13 @@ function displayOAuthApps(data) {
           <p>Redirect URI: ${app.redirectUri}</p>
        </div>
       `;
-      container.appendChild(appBox);
+    container.appendChild(appBox);
   });
 }
 
-
-
 function handleResponse(response) {
   if (response.status === 200) {
-    console.log(response);
+  } else if (response.status === 404) {
   } else if (response.status === 460) {
     return handle460Error();
   } else if (response.status === 465) {
@@ -174,35 +161,33 @@ function handleResponse(response) {
   }
 }
 
-
 function handle460Error() {
-  displayError('Error: Invalid redirect URI')
+  displayError("Error: Invalid redirect URI");
 }
 
 function handle465Error() {
-  displayError('Error: You have no access to this resource')
+  displayError("Error: You have no access to this resource");
 }
-
-
 
 function handleError() {
-  displayError('Something went wrong')
+  displayError("Something went wrong");
 }
 
-
 var currentURL = window.location.origin;
-document.getElementById('authorization-url').textContent = currentURL + "/api/oauth/authorize";
-document.getElementById('token-url').textContent = currentURL + "/api/oauth/token";
-document.getElementById('token-check-url').textContent = currentURL + "/api/oauth/check_token";
-document.getElementById('userinfo-uri').textContent = currentURL + "/api/oauth/userinfo";
-
-
+document.getElementById("authorization-url").textContent =
+  currentURL + "/api/oauth/authorize";
+document.getElementById("token-url").textContent =
+  currentURL + "/api/oauth/token";
+document.getElementById("token-check-url").textContent =
+  currentURL + "/api/oauth/check_token";
+document.getElementById("userinfo-uri").textContent =
+  currentURL + "/api/oauth/userinfo";
 
 function displaySuccess(successMessage) {
   successBox.textContent = successMessage;
   document.body.appendChild(successBox);
   setTimeout(() => {
-      successBox.remove();
+    successBox.remove();
   }, 2500);
 }
 
@@ -210,6 +195,6 @@ function displayError(errorMessage) {
   errorBox.textContent = errorMessage;
   document.body.appendChild(errorBox);
   setTimeout(() => {
-      errorBox.remove();
+    errorBox.remove();
   }, 2500);
 }
