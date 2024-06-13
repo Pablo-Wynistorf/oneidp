886ac47d5d694aab382c725052374719efa27c95
Pablo-Wynistorf <pablo.wynistorf@swisscom.com>
2024-06-13 16:12:12 +0200

    updated the openid well-known infos docker

--- src/routes/api/oauth/openid-info.js
+++ src/routes/api/oauth/openid-info.js
@@ -11,11 +11,13 @@ router.get('/', (req, res) => {
     token_endpoint: `${URL}/api/oauth/token`,
     userinfo_endpoint: `${URL}/api/oauth/userinfo`,
     jwks_uri: `${URL}/.well-known/jwks.json`,
-    response_types_supported: ['code', 'token', 'id_token'],
+    response_types_supported: ['code', 'token', 'id_token', 'code token', 'code id_token', 'token id_token', 'code token id_token'],
+    subject_types_supported: ['public'],
     id_token_signing_alg_values_supported: ['RS256'],
     token_endpoint_auth_signing_alg_values_supported: ['RS256'],
     scopes_supported: ["openid"],
-    grant_types_supported: ["authorization_code"],
+    token_endpoint_auth_methods_supported: ["client_secret_post"], 
+    grant_types_supported: ["authorization_code", "refresh_token"],
   };
   res.json(metadata);
 });
