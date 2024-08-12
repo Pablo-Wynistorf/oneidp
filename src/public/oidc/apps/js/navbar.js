6ed598da6a0d6b0e5c4bce3501bf6456cf8f2d26
Pablo-Wynistorf <pablo.wynistorf@swisscom.com>
2024-08-12 10:53:40 +0200

    fixed navbar on mobile docker

--- src/public/oidc/apps/js/navbar.js
+++ src/public/oidc/apps/js/navbar.js
@@ -1,31 +1,36 @@
-document.getElementById('mobile-open-menu').addEventListener('click', function() {
-    toggleMenu(true);
-});
+document.addEventListener('DOMContentLoaded', () => {
+    const openMenuButton = document.getElementById('mobile-open-menu');
+    const closeMenuButton = document.getElementById('mobile-close-menu');
+    const sidebar = document.getElementById('sidebar');
+    const settingsLogoutButtons = document.getElementById('settings-logout-buttons');
+    const navItems = document.getElementById('nav-items');
 
-document.getElementById('mobile-close-menu').addEventListener('click', function() {
-    toggleMenu(false);
-});
-
-function toggleMenu(isOpen) {
-    var navItems = document.getElementById('nav-items');
-    var mobileMenuIcon = document.getElementById('mobile-open-menu');
-    var closeMenuIcon = document.getElementById('mobile-close-menu');
-    var settingsItem = document.getElementById('settings-item');
-    var logoutItem = document.getElementById('logout-item');
-
-    if (isOpen) {
-        navItems.classList.remove('hidden');
-        navItems.classList.add('block');
-        mobileMenuIcon.classList.add('hidden');
-        closeMenuIcon.classList.remove('hidden');
-        settingsItem.classList.remove('hidden');
-        logoutItem.classList.remove('hidden');
-    } else {
-        navItems.classList.add('hidden');
-        navItems.classList.remove('block');
-        mobileMenuIcon.classList.remove('hidden');
-        closeMenuIcon.classList.add('hidden');
-        settingsItem.classList.add('hidden');
-        logoutItem.classList.add('hidden');
+    // Toggle mobile menu
+    function toggleMenu(isOpen) {
+        if (isOpen) {
+            navItems.classList.remove('hidden');
+            navItems.classList.add('block');
+            openMenuButton.classList.add('hidden');
+            closeMenuButton.classList.remove('hidden');
+            settingsLogoutButtons.classList.remove('hidden');
+        } else {
+            navItems.classList.add('hidden');
+            navItems.classList.remove('block');
+            openMenuButton.classList.remove('hidden');
+            closeMenuButton.classList.add('hidden');
+            settingsLogoutButtons.classList.add('hidden');
+        }
     }
-}
+
+    // Handle open menu button click
+    openMenuButton.addEventListener('click', () => {
+        sidebar.classList.add('open');
+        toggleMenu(true);
+    });
+
+    // Handle close menu button click
+    closeMenuButton.addEventListener('click', () => {
+        sidebar.classList.remove('open');
+        toggleMenu(false);
+    });
+});
