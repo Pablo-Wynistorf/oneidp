08a44ca25d2a7233bee082583c7965ae1934f5eb
Pablo-Wynistorf <pablo.wynistorf@swisscom.com>
2025-01-13 10:49:19 +0100

    updated health check docker

--- src/routes/api/health/health.js
+++ src/routes/api/health/health.js
@@ -6,21 +6,26 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
-    const result = await userDB.findOne({});
-    const error = "Database connection error, or not initialized."
-    if (!result) {
+    const dbStats = await userDB.db.command({ ping: 1 });
+    if (dbStats.ok !== 1) {
+      const error = 'Database connection error, or not initialized.';
       notifyError(error);
-      return res.status(500).json({ error: error });
+      return res.status(500).json({ error });
     }
+
     const response = await fetch('https://google.com', { method: 'GET' });
     if (!response.ok) {
       notifyError('Application has no connection to the internet');
       return res.status(500).json({ error: 'Application has no connection to the internet' });
     }
+
     res.status(200).json({ status: 'ok' });
   } catch (error) {
-    notifyError(error);
-    res.status(500).json({ error: 'Application has encountered an error:', details: error.toString() });
+    notifyError(error.message || error.toString());
+    res.status(500).json({
+      error: 'Application has encountered an error.',
+      details: error.message || error.toString(),
+    });
   }
 });
 
